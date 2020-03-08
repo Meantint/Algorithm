2997cba805029cb1f3eebe830e471986887cdc97
Meantint <50372451+Meantint@users.noreply.github.com>
2020-03-08 22:39:38 +0900

    Update Prim Algorithm.cpp

--- Prim Algorithm.cpp
+++ Prim Algorithm.cpp
@@ -1,6 +1,5 @@
 // 프림 알고리즘 ( Prim's algorithm )
-// weekly ps
-// https://www.weeklyps.com/entry/%ED%94%84%EB%A6%BC-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-Prims-algorithm
+// 출처 : https://www.weeklyps.com/entry/%ED%94%84%EB%A6%BC-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-Prims-algorithm [weekly ps]
 
 #include <iostream>
 #include <vector>
