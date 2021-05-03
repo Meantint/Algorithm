f01a13f7f1948d428bbd6b68d4b8e5d6506f5117
meantint <mistinca@naver.com>
2021-05-03 23:58:51 +0900

    Update Comment and Code

--- BFS/BFS.cpp
+++ BFS/BFS.cpp
@@ -5,12 +5,13 @@
 using namespace std;
 
 int v, e;
-int visitOrder = 1;       // 방문 순서 저장
 vector<bool> isVisited;   // Node 방문 여부 저장
 vector<vector<int>> adj;  // 간선 정보 저장
 
-void BFS(int src)
+vector<int> BFS(int src)
 {
+    vector<int> visitOrder;
+
     queue<int> q;  // 방문 순서 담아두는 queue, FIFO(First In First Out)
 
     // queue에 src를 넣어주고
@@ -22,7 +23,7 @@ void BFS(int src)
         int cur = q.front();  // 가장 오래된 원소를 뽑아낸다.
         q.pop();
 
-        cout << "Visit #" << visitOrder++ << " : " << cur << '\n';  // Print
+        visitOrder.push_back(cur);  // 방문 중인 node를 visitOrder에 추가한다.
 
         int size = adj[cur].size();  // cur 노드에 연결되어 있는 다른 노드의 개수
         for (int i = 0; i < size; ++i) {
@@ -38,6 +39,8 @@ void BFS(int src)
             }
         }
     }
+
+    return visitOrder;
 }
 
 int main()
@@ -54,7 +57,13 @@ int main()
         adj[v2].push_back(v1);  // v2 to v1
     }
 
-    BFS(0);
+    vector<int> visitOrder = BFS(0);
+    cout << "Visit : ";
+    int vSize = visitOrder.size() - 1;  // 마지막 -> 제거하기 위해 -1
+    for (int i = 0; i < vSize; ++i) {
+        cout << visitOrder[i] << " -> ";
+    }
+    cout << visitOrder.back() << '\n';
 
     return 0;
 }
